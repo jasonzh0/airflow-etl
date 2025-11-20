a203f851ee5b210e36d04c3a2c1bd58067ee25d4
Jason Zhang <zjjc123@hotmail.com>
2025-11-20 16:05:21 -0500

    fix port forward

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -15,7 +15,7 @@ NAMESPACE="airflow"
 RELEASE_NAME="airflow"
 LOCAL_PORT=8080
 
-echo -e "${BLUE}Setting up port forwarding to Airflow webserver...${NC}"
+echo -e "${BLUE}Setting up port forwarding to Airflow API server...${NC}"
 echo ""
 
 # Check if kubectl is configured
@@ -31,10 +31,10 @@ if ! kubectl get namespace "$NAMESPACE" &> /dev/null; then
     exit 1
 fi
 
-# Get webserver service name
-WEBSERVER_SERVICE="${RELEASE_NAME}-webserver"
-if ! kubectl get svc "$WEBSERVER_SERVICE" -n "$NAMESPACE" &> /dev/null; then
-    echo -e "${RED}Error: Service '$WEBSERVER_SERVICE' not found in namespace '$NAMESPACE'${NC}"
+# Get API server service name (Airflow 3 uses api-server instead of webserver)
+API_SERVICE="${RELEASE_NAME}-api-server"
+if ! kubectl get svc "$API_SERVICE" -n "$NAMESPACE" &> /dev/null; then
+    echo -e "${RED}Error: Service '$API_SERVICE' not found in namespace '$NAMESPACE'${NC}"
     echo "Make sure Airflow is deployed"
     exit 1
 fi
@@ -53,7 +53,7 @@ if lsof -Pi :$LOCAL_PORT -sTCP:LISTEN -t >/dev/null 2>&1 ; then
     fi
 fi
 
-echo -e "${BLUE}Forwarding local port $LOCAL_PORT to Airflow webserver...${NC}"
+echo -e "${BLUE}Forwarding local port $LOCAL_PORT to Airflow API server...${NC}"
 echo -e "${YELLOW}Press Ctrl+C to stop port forwarding${NC}"
 echo ""
 echo -e "${GREEN}Access Airflow UI at: http://localhost:$LOCAL_PORT${NC}"
@@ -62,6 +62,6 @@ echo ""
 # Port forward
 kubectl port-forward \
     -n "$NAMESPACE" \
-    svc/"$WEBSERVER_SERVICE" \
+    svc/"$API_SERVICE" \
     $LOCAL_PORT:8080
 
